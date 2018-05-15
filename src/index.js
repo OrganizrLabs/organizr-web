b7f47e6306a2e35443add766db224e9c84ab976a
Zachary Marion <zfm2@duke.edu>
2018-05-15 13:45:43 -0400

    Change router type to <BrowserRouter />

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Redirect,
   Switch,
   Route
